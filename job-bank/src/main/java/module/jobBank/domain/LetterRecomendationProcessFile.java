3498a8df38e98a6f611234712a65584d1ca68889
João Antunes <joao.antunes@tagus.ist.utl.pt>
2013-01-15 14:54:28 +0000

    Refactor: Workflow & FileManagement integration . Workflow-2.0  & minor version of modules that depend on them are committed. See http://wiki.dsi.ist.utl.pt/Projetos/GestaoDocumental/IntegracaoSistemasInformacao for migration info

--- job-bank/src/main/java/module/jobBank/domain/LetterRecomendationProcessFile.java
+++ job-bank/src/main/java/module/jobBank/domain/LetterRecomendationProcessFile.java
@@ -1,10 +1,16 @@
 package module.jobBank.domain;
 
+import module.workflow.domain.ProcessDocumentMetaDataResolver;
+import module.workflow.domain.ProcessFile;
 import pt.ist.bennu.core.util.ClassNameBundle;
 
 @ClassNameBundle(bundle = "resources/JobBankResources")
 public class LetterRecomendationProcessFile extends LetterRecomendationProcessFile_Base {
 
+    @Override
+    public ProcessDocumentMetaDataResolver<? extends ProcessFile> getMetaDataResolver() {
+	return new CurriculumProcess.CurriculumRelatedFileMetadataResolver();
+    }
     public LetterRecomendationProcessFile(String displayName, String filename, byte[] content) {
 	super();
 	if (content != null) {
